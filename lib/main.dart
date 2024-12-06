aa4cb517ae0c90413a2f2adcd249fe851d341e79
Mozain120803 <atoriqzelfi999@gmail.com>
2024-12-06 15:06:07 +0700

    pertemuan 9

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:wisata_mobile_5/materimodulscreens/formscreen6.dart';
+import 'package:wisata_mobile_5/screens/homescreen.dart';
+import 'package:wisata_mobile_5/screens/splashscreen.dart';
+
+void main() {
+  runApp(TravelApp());
+}
+
+class TravelApp extends StatelessWidget {
+  const TravelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // home: FormScreen6(),
+      home: Homescreen(),
+    );
+  }
+}
